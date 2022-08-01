58c5782ca5e4de2570c751fa096b92a9ab88a3d7
Florzito <netojose42@gmail.com>
2022-08-01 11:44:38 -0300

    add search

--- js/script.js
+++ js/script.js
@@ -2,16 +2,28 @@ const urlApi = 'https://pokeapi.co/api/v2/pokemon'
 
 
 async function testeApi() {
+    const response = await returnResponse()
+    const dbPokemon = await returnList(response)
+
+    document.getElementById('content').innerHTML = dbPokemon.map(e => e).join('')
+}
+
+
+async function returnResponse() {
     const response = await fetch(urlApi)
         .then(e => e.json())
         .then(e => e.results)
         .then(e => e.map(e => e.url))
+    return response
+}
 
+
+async function returnList(response) {
     const dbPokemon = []
     for (const pokemon of response) {
         const responseFor = await fetch(pokemon)
             .then(e => e.json())
-            .then(e => e)
+
         dbPokemon.push(`
                 <a href="#" class="col-3 ">
                     <h3>${responseFor.name}</h3>
@@ -23,8 +35,17 @@ async function testeApi() {
                 </a>
         `)
     }
-    document.getElementById('content').innerHTML = dbPokemon.map(e => e).join('')
-
+    return dbPokemon
 }
 
 window.onload = testeApi()
+
+
+async function searchPokemon() {
+    const search = document.getElementById('search-pokemon').value
+    const response = await returnResponse()
+    const dbPokemon = await returnList(response)
+    const searchPokemon = dbPokemon.filter(e => e.includes(search))
+
+    document.getElementById('content').innerHTML = searchPokemon.map(e => e).join('')
+}
