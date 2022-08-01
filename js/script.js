a55661e004b16c7829bdc57d7baa0f959ac3770f
Florzito <netojose42@gmail.com>
2022-07-31 22:02:19 -0300

    up project

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const urlApi = 'https://pokeapi.co/api/v2/pokemon'
+
+
+async function testeApi() {
+    const response = await fetch(urlApi)
+        .then(e => e.json())
+        .then(e => e.results)
+        .then(e => e.map(e => e.url))
+
+    const dbPokemon = []
+    for (const pokemon of response) {
+        const responseFor = await fetch(pokemon)
+            .then(e => e.json())
+            .then(e => e)
+        dbPokemon.push(`
+                <a href="#" class="col-3 ">
+                    <h3>${responseFor.name}</h3>
+                    <img src="${responseFor.sprites.other.home.front_default}"./img/1.png" alt=${responseFor.name}>
+                    <p>Habilidades:</p>
+                    <ul>
+                        ${responseFor.abilities.map(e => `<li>${e.ability.name}</li>`).join('')}
+                    </ul>
+                </a>
+        `)
+    }
+    document.getElementById('content').innerHTML = dbPokemon
+}
+
+testeApi()
